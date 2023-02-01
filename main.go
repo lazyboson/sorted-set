16e486624ed758ddac6edc022ead56fc2c48c64a
lazyboson <lazyboson@gmail.com>
2023-02-01 10:36:29 +0530

    added lua script

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"strconv"
 )
 
 type Data struct {
-	status string `json:"status"`
-	val    string `json:"val"`
+	Status string `json:"status"`
+	Val    string `json:"val"`
 }
 
 func main() {
@@ -18,7 +19,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	key := "my_sorted_set"
+	key := "myset"
 	err = addToSS(key, "ashutosh", "0")
 	if err != nil {
 		fmt.Println(err)
@@ -35,33 +36,34 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = addToSS(key, "rahoul", "4")
+	err = addToSS(key, "rahul", "4")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	_ = &Data{
-		status: "completed",
-		val:    "5",
-	}
-
-	err = AddKeyVal("ashutosh", "ashu")
+	data := &Data{
+		Status: "completed",
+		Val:    "5"}
+	err = AddKeyVal("ashutosh", data)
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = AddKeyVal("pooja", "ashu")
+	err = AddKeyVal("pooja", data)
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = AddKeyVal("yogita", "ashu")
+	err = AddKeyVal("yogita", data)
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = AddKeyVal("priya", "ashu")
+	err = AddKeyVal("priya", data)
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = AddKeyVal("rahul", "ashyu")
+	data = &Data{
+		Status: "complete",
+		Val:    "5"}
+	err = AddKeyVal("rahul", data)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -74,7 +76,8 @@ func main() {
     	local score = members_with_scores[i + 1]
     	local value = redis.call('GET', member)
     	if value then
-    	    if value == "ashu" then
+			local data = cjson.decode(value)
+    	    if data.status == "completed" then
     	        table.insert(result, { member, score })
     	        if #result == tonumber(KEYS[2]) then
     	            break
@@ -86,7 +89,7 @@ func main() {
 	return result
 	`)
 
-	result, err := redis.Values(script.Do(conn, key, 2))
+	result, err := redis.Values(script.Do(conn, key, 8))
 	if err != nil {
 		// handle error
 		fmt.Println(err)
@@ -119,16 +122,16 @@ func addToSS(key, member, score string) error {
 	return nil
 }
 
-func AddKeyVal(key string, data string) error {
+func AddKeyVal(key string, data *Data) error {
 	conn, err := redis.Dial("tcp", "localhost:6379")
 	if err != nil {
 		// handle error
 	}
 	defer conn.Close()
 
-	//dataBytes, _ := json.Marshal(data)
+	dataBytes, _ := json.Marshal(data)
 
-	_, err = redis.String(conn.Do("SET", key, data))
+	_, err = redis.String(conn.Do("SET", key, dataBytes))
 	if err != nil {
 		return err
 	}
